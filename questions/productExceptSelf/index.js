6ed0b683057b46e3607b3750a5bd447030c2d685
Brian <briancmuhammad@yahoo.com>
2024-09-02 13:13:59 -0700

    finished product except self challenge

--- questions/productExceptSelf/index.js
+++ questions/productExceptSelf/index.js
@@ -14,6 +14,37 @@
  */
 
 const productExceptSelf = (nums) => {
+//const leftProducts = [] removed for constant space
+
+const results = []
+
+results[0] = 1   //replace with results for constant space leftProducts[0] = 1  
+//rightProducts[nums.length -1] = 1
+
+for(let i = 1; i < nums.length; ++i){
+  //replace with results for constant space   leftProducts[i] = leftProducts[i -1] * nums[i -1]
+  results[i] = results[i -1] * nums[i -1]
+}
+
+let rightProducts  = 1 
+for (let i = nums.length - 1; i >= 0; --i) {// length - 2
+  results[i] = results[i] * rightProducts// removed for constant space rightProducts[i] = rightProducts[i + 1] * nums[i + 1]
+  rightProducts =  rightProducts * nums[i]  //add for constant space
+}
+
+// dont need for constant space
+//for(let i = 0; i < nums.length; ++i) {
+ //   results[i] =   results[i] * rightProducts[i] //replace with results for constant space leftProducts[i] * rightProducts[i]
+//}
+
+
+
+return results;
+
+
+
+//need to do in constant space. we are storing values
+
 
 };
 
