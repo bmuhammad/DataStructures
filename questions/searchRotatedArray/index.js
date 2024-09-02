e37d872e0dcae7df82310a4d5ea72fe2f4ac612a
Brian <briancmuhammad@yahoo.com>
2024-09-02 14:58:29 -0700

    finished search Rotated Array challenge

--- questions/searchRotatedArray/index.js
+++ questions/searchRotatedArray/index.js
@@ -12,8 +12,35 @@
  * search([3, 4, 2, 0], 4) -> 1
  * 
  */
-
+//allways consider binary search when time complexity is greater than O(n)
 const search = (nums, target) => {
+    let l = 0;
+    let r = nums.length -1;
+
+    while (l <= r){
+        let mid = Math.floor((l + r) / 2);
+
+        if(nums[mid] === target)
+            return mid
+
+        if(nums[mid] < nums[r]){
+            if(target < nums[mid] || target > nums[r]){
+                r= mid - 1
+            }
+            else{
+                l = mid + 1
+            }
+        }
+        else{
+            if(target > nums[mid] || target < nums[l]){
+                l = mid + 1
+            }
+            else{
+                r = mid - 1
+            }
+        }
+    }
+    return " not found"
 
 }
 
