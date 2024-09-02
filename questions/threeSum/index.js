2c8b66b4edf031d701441eae80aeff551f9130d0
Brian <briancmuhammad@yahoo.com>
2024-09-02 12:24:47 -0700

    Finished threesum. complext while loop inside of while loop inside for loop

--- questions/threeSum/index.js
+++ questions/threeSum/index.js
@@ -11,8 +11,42 @@
  * threeSum([0, 0, 0, 0]) = [[0, 0, 0]]
  */
 
+//notes. use sorting and pointer for best solution o(n2)
+
 function threeSum(nums) {
-  
+  nums.sort((a, b) => a - b);
+  const results = [];
+
+  for (i = 0; i < nums.length - 2; ++i) {
+    if (nums[i] === nums[i - 1]) {
+      continue;
+    }
+
+    let l = i + 1;
+    let r = nums.length - 1;
+
+    while (l < r) {
+      const sum = nums[i] + nums[l] + nums[r];
+
+      if (sum > 0) {
+        r--;
+      }
+
+      if (sum < 0) {
+        l++;
+      }
+
+      if (sum === 0) {
+        results.push([nums[i], nums[l], nums[r]]);
+        l++;
+        while (nums[l] === nums[l - 1] && l < r) {
+          l++;
+        }
+      }
+    }
+  }
+
+  return results;
 }
 
 //DO NOT EDIT BELOW THIS LINE
